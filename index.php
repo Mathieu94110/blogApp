79fff9b66cab7d63d2a173a90505f3b453c88c8c
mathieu enault <me94110@gmail.com>
2024-02-22 15:32:09 +0100

    article details page done

--- index.php
+++ index.php
@@ -60,12 +60,12 @@ if (file_exists($filename)) {
                             <h2><?= $cat ?></h2>
                             <div class="articles-container">
                                 <?php foreach ($articlePerCategories[$cat] as $a) : ?>
-                                    <div class="article block">
+                                    <a href="/show-article.php?id=<?= $a['id'] ?>" class="article block">
                                         <div class="overflow">
                                             <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
                                         </div>
                                         <h3><?= $a['title'] ?></h3>
-                                    </div>
+                                    </a>
                                 <?php endforeach; ?>
                             </div>
                         <?php endforeach; ?>
@@ -73,12 +73,12 @@ if (file_exists($filename)) {
                         <h2><?= $selectedCat ?></h2>
                         <div class="articles-container">
                             <?php foreach ($articlePerCategories[$selectedCat] as $a) : ?>
-                                <div class="article block">
+                                <a href="/show-article.php?id=<?= $a['id'] ?>" class="article block">
                                     <div class="overflow">
                                         <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
                                     </div>
                                     <h3><?= $a['title'] ?></h3>
-                                </div>
+                                </a>
                             <?php endforeach; ?>
                         </div>
                     <?php endif; ?>
