1f23a46993fc5944a88e4a2c96053b20f200001a
mathieu enault <me94110@gmail.com>
2024-03-15 19:27:52 +0100

    move article interaction logic into ArticleDB on database models

--- show-article.php
+++ show-article.php
@@ -1,16 +1,12 @@
 <?php
-
-$pdo = require_once 'database.php';
-$statement = $pdo->prepare('SELECT * FROM article WHERE id = :id');
+$articleDB = require_once __DIR__ . '/database/models/ArticleDB.php';
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $id = $_GET['id'] ?? '';
 
 if (!$id) {
-    header('Location: /');
+  header('Location: /');
 } else {
-    $statement->bindValue(':id', $id);
-    $statement->execute();
-    $article = $statement->fetch();
+  $article = $articleDB->fetchOne($id);
 }
 ?>
 
@@ -18,29 +14,29 @@ if (!$id) {
 <html lang="fr">
 
 <head>
-    <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="/public/css/show-article.css">
-    <title>Article</title>
+  <?php require_once 'includes/head.php' ?>
+  <link rel="stylesheet" href="/public/css/show-article.css">
+  <title>Article</title>
 </head>
 
 <body>
-    <div class="container">
-        <?php require_once 'includes/header.php' ?>
-        <div class="content">
-            <div class="article-container">
-                <a class="article-back" href="/">Retour à la liste des articles</a>
-                <div class="article-cover-img" style="background-image:url(<?= $article['image'] ?>)"></div>
-                <h1 class="article-title"><?= $article['title'] ?></h1>
-                <div class="separator"></div>
-                <p class="article-content"><?= $article['content'] ?></p>
-                <div class="action">
-                    <a class="btn btn-secondary" href="/delete-article.php?id=<?= $article['id'] ?>">Supprimer</a>
-                    <a class="btn btn-primary" href="/form-article.php?id=<?= $article['id'] ?>">Editer l'article</a>
-                </div>
-            </div>
+  <div class="container">
+    <?php require_once 'includes/header.php' ?>
+    <div class="content">
+      <div class="article-container">
+        <a class="article-back" href="/">Retour à la liste des articles</a>
+        <div class="article-cover-img" style="background-image:url(<?= $article['image'] ?>)"></div>
+        <h1 class="article-title"><?= $article['title'] ?></h1>
+        <div class="separator"></div>
+        <p class="article-content"><?= $article['content'] ?></p>
+        <div class="action">
+          <a class="btn btn-secondary" href="/delete-article.php?id=<?= $article['id'] ?>">Supprimer</a>
+          <a class="btn btn-primary" href="/form-article.php?id=<?= $article['id'] ?>">Editer l'article</a>
         </div>
-        <?php require_once 'includes/footer.php' ?>
+      </div>
     </div>
+    <?php require_once 'includes/footer.php' ?>
+  </div>
 
 </body>
 
