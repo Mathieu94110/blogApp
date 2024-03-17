39dba0ddffc78659640d40d65d65db417a7f2d86
mathieu enault <me94110@gmail.com>
2024-03-17 21:29:29 +0100

    adding authentication including article db, security for login and navigation

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,27 @@
+<?php
+$currentUser = $currentUser ?? false;
+?>
+
 <header>
-    <a href="/" class="logo">BlogApp</a>
+    <a href="/" class="logo">Blog</a>
     <ul class="header-menu">
-        <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
-            <a href="/form-article.php">Écrire un article</a>
-        </li>
+        <?php if ($currentUser) : ?>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
+                <a href="/form-article.php">Écrire un article</a>
+            </li>
+            <li class="<?= $_SERVER['REQUEST_URI'] === '/profile.php' ? 'active' : '' ?> header-profile">
+                <a href="/profile.php"><?= $currentUser['firstname'][0] . $currentUser['lastname'][0] ?></a>
+            </li>
+            <li>
+                <a href="/auth-logout.php">Déconnexion</a>
+            </li>
+        <?php else : ?>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? 'active' : '' ?>>
+                <a href="/auth-register.php">Inscription</a>
+            </li>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? 'active' : '' ?>>
+                <a href="/auth-login.php">Connexion</a>
+            </li>
+        <?php endif; ?>
     </ul>
 </header>
